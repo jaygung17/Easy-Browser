92df59d68512eaea023909168d46840b15ba0f5f
Novan Agung Waskito <68317534+jaygung17@users.noreply.github.com>
2022-05-25 19:27:15 +0700

    Initial Commit

---
+++ Easy Browser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Easy Browser
+//
+//  Created by Agung Luo on 25/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
